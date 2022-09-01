fcbba8dae6a29db3430d41e76715730633dae6e3
Kato <112272544+SergioDSGT@users.noreply.github.com>
2022-09-01 20:58:34 -0300

    update files
    yeeey
    maicro fix plss

--- papaApplication01.cpp
+++ papaApplication01.cpp
@@ -1,20 +1,27 @@
-// papaApplication01.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+//#include <cctype>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int timesRepeating01 = 0;
+	char repeatingString[512];
+
+	cout << "coloque o numero de vezes que queira repetir o exemplo." << endl;
+	cin >> timesRepeating01;
+	cout << "coloque a string que queres repetir " << timesRepeating01 << " vezes:" << endl;
+	std::cin.getline(repeatingString, 512, '=');
+	system("cls");
+	//cin >> line2repeat;
+
+	for (int i = 0; i < timesRepeating01; i++)
+	{
+		cout << repeatingString << endl;
+	}
+
+	cout << "pronto! a string " << repeatingString << " foi repetida " << timesRepeating01 << " vezes.";
+	// cout << "fora do loop!" << endl;
+
+	return 777;
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
---
+++ temporarioTestes.cpp
@@ -0,0 +1,23 @@
+/* #include <iostream>
+using namespace std;
+
+
+int main()
+{
+	string stringTeste001 = "";
+	cout << "input stringTeste001" << endl;
+	cin >> stringTeste001;
+	cout << "break 123 123 123" << endl;
+	cout << stringTeste001;
+
+	return 0;
+} 
+
+int main()
+{
+	char charTeste001[512];
+	cout << "insira os char mt loko ae" << endl;
+	cin.getline(charTeste001, 512);
+	cout << "out insane ae" << charTeste001 << "jorgeeeee";
+	return 0;
+} */
