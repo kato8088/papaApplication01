f19d886d1ca018f7bf1abb44e7c048bb277e7ed7
Kato <112272544+SergioDSGT@users.noreply.github.com>
2022-09-03 00:23:00 -0300

    upload final 030922
    update 2 release do source code, so falta compilar :)

--- papaApplication01.cpp
+++ papaApplication01.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
 //#include <cctype>
 
-using namespace std;
+// using namespace std;
 
 int main()
 {
+	using namespace std;
+
 	int timesRepeating01 = 0;
-	char repeatingString[512];
+	char string2repeat[512];
 
-	cout << "coloque o numero de vezes que queira repetir o exemplo." << endl;
-	cin >> timesRepeating01;
-	cout << "coloque a string que queres repetir " << timesRepeating01 << " vezes:" << endl;
-	std::cin.getline(repeatingString, 512, '=');
-	system("cls");
-	//cin >> line2repeat;
-
-	for (int i = 0; i < timesRepeating01; i++)
+	while(true)
 	{
-		cout << repeatingString << endl;
-	}
+		try
+		{
+			//restart:
+			cout << "coloque o numero de vezes que queira repetir o exemplo." << endl;
+			cin >> timesRepeating01;
+			if (cin.fail())
+			{
+				throw 404;
+			}
+			cout << "coloque a string que queres repetir " << timesRepeating01 << " vezes:" << endl;
+			cin.ignore();
+			cin.getline(string2repeat, 512);
+			system("cls");
+			//cin >> string2repeat;
 
-	cout << "pronto! a string " << repeatingString << " foi repetida " << timesRepeating01 << " vezes.";
-	// cout << "fora do loop!" << endl;
+			for (int forInt01 = 0; forInt01 < timesRepeating01; forInt01++)
+			{
+				cout << string2repeat << endl;
+			}
+
+			cout << "" << endl;
+			cout << "pronto! a string " << string2repeat << " foi repetida " << timesRepeating01 << " vezes." << endl;
+			cout << "quer repetir o processo com outra string e numero de repeticoes?" << endl;
+			system("pause");
+			system("cls");
+			// adicionar dps uma opcao de escolha s/n - caso s, retornar ao comeco da aplicacao / caso n, haltar o programa (return)
+			// pause temporario at the moment redireciona o console para o while loop
+		}
+		catch (...)
+		{
+			cin.clear();
+			cin.ignore();
+			system("cls");
+			cout << "tente novamente! insira um numero valido, de 1 - 512 caracteres!" << endl << endl;
+			continue;
+		}
+	}
 
 	return 777;
 }
