36781a35fc7ff1aa4933b0756af9d01efd87727a
Kato <112272544+SergioDSGT@users.noreply.github.com>
2022-09-05 23:33:22 -0300

    updated files
    05-09-2022 - adicionadas diversas funcoes :DD

--- papaApplication01.cpp
+++ papaApplication01.cpp
@@ -9,13 +9,14 @@ int main()
 
 	int timesRepeating01 = 0;
 	char string2repeat[512];
+	string restartApplication = "";
 
+	// issue #02: mais de um argumento no primeiro cin faz o programa skippar o segundo cin e automaticamente lancar o resultado
 	while(true)
 	{
 		try
 		{
-			//restart:
-			cout << "coloque o numero de vezes que queira repetir o exemplo." << endl;
+			cout << "coloque o numero de vezes que queira repetir a string." << endl;
 			cin >> timesRepeating01;
 			if (cin.fail())
 			{
@@ -24,7 +25,8 @@ int main()
 			cout << "coloque a string que queres repetir " << timesRepeating01 << " vezes:" << endl;
 			cin.ignore();
 			cin.getline(string2repeat, 512);
-			system("cls");
+			cout << endl;
+			//	system("cls");
 			//cin >> string2repeat;
 
 			for (int forInt01 = 0; forInt01 < timesRepeating01; forInt01++)
@@ -35,11 +37,10 @@ int main()
 			cout << "" << endl;
 			cout << "pronto! a string " << string2repeat << " foi repetida " << timesRepeating01 << " vezes." << endl;
 			cout << "quer repetir o processo com outra string e numero de repeticoes?" << endl;
-			system("pause");
-			system("cls");
-			// adicionar dps uma opcao de escolha s/n - caso s, retornar ao comeco da aplicacao / caso n, haltar o programa (return)
-			// pause temporario at the moment redireciona o console para o while loop
+			cout << "digite 'sim' para reiniciar o programa e 'nao' para sair." << endl;
+
 		}
+
 		catch (...)
 		{
 			cin.clear();
@@ -48,7 +49,36 @@ int main()
 			cout << "tente novamente! insira um numero valido, de 1 - 512 caracteres!" << endl << endl;
 			continue;
 		}
+
+	returnElse1:
+		cin >> restartApplication;
+		//debug >
+		//cout << "debug" << endl;
+		//cout << restartApplication << endl;
+		//system("pause");
+
+		if (restartApplication == "sim")
+			{
+				//cout << "input = sim";
+				system("cls");
+				continue;
+			}
+			if (restartApplication == "nao")
+			{
+				cout << "see you next time! :D" << endl;
+				return 727;
+			}
+			else
+			{
+			//	cout << "tem q codar ainda, mas resumidamente significa q fodeu xd" << endl;
+				system("cls");
+				cout << "tente novamente! insira 'sim' para reiniciar ou 'nao' para sair do programa." << endl;
+				goto returnElse1;
+			}
+
+
 	}
+
 
 	return 777;
 }
--- temporarioTestes.cpp
+++ temporarioTestes.cpp
@@ -20,4 +20,54 @@ int main()
 	cin.getline(charTeste001, 512);
 	cout << "out insane ae" << charTeste001 << "jorgeeeee";
 	return 0;
-} */
+}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		cout << "while :D" << endl;
+		break;
+	}
+	cout << "fora do loop :DD" << endl;
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int jorge = 5;
+	float returnQuestionMark = 0.0;
+
+	while (jorge == 5)
+	{
+		cout << "looping :D" << endl;
+		cout << "enter integer value:" << endl;
+		cout << "qq coisa maior que zero quebra o loop" << endl;
+		cin >> jorge;
+	}
+	while (true)
+	{
+		cout << "looping :D" << endl;
+		cout << "enter integer value:" << endl;
+		cout << "o valor 5 quebra o loop" << endl;
+		cin >> jorge;
+		if (jorge == 5)
+			break;
+	}
+	cout << "fora do loop, voltar?" << endl;
+	cout << "para voltar digite 420" << endl;
+	cin >> returnQuestionMark;
+	while(jorge == 5)
+	if (returnQuestionMark == 420)
+	{
+		int jorge = 0;
+		float returnQuestionMark = 0;
+	}
+
+	return 2022;
+}
+*/
